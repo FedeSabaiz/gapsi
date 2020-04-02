17e5042446dc80b82219042a0b0ecf92d63e6689
Federico <federico.gomez.sa@gmail.com>
2020-04-02 11:09:05 -0600

    Initialize project Gapsi

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './views/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <h1>Iniciando prueba</h1>
+    );
+}
+ 
+export default Home;
