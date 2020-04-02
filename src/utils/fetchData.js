e7cb55e58cea95c7336085cf30930b7d91ec9116
Federico <federico.gomez.sa@gmail.com>
2020-04-02 15:12:52 -0600

    finish request GET, POST

---
+++ src/utils/fetchData.js
@@ -0,0 +1,26 @@
+export const fetchUser = async () => {
+    try {
+        const response = await fetch('https://node-red-nxdup.mybluemix.net/visitor', {
+            method: 'POST'
+        });
+        const res = await response.json();
+        // console.log(res);
+        return res;
+    } catch (error) {
+        console.error('No se completó la petición', error);
+        
+    }
+}
+
+export const fetchProduct = async ({categoria, pag}) => {
+    try {
+        const response = await fetch(`https://node-red-nxdup.mybluemix.net/productos/${categoria}/${pagina}`, {
+            method: 'GET'
+        });
+        const res = await response.json();
+        return res;
+    } catch (error) {
+        console.log('No se pudo completar la petición', error);
+        
+    }
+}
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchUser } from '../utils/fetchData';
 
 const Home = () => {
+
+    // const [user, loading] = useFetchHook();
+    const [data, getData] = useState({});
+
+    useEffect( () => {
+        const res = async() => {
+            let x = await fetchUser();
+            getData(x);
+
+        }
+        res();
+    }, []);
+
+    console.log(data)
+    // console.log(user);
+
     return (
         <h1>Iniciando prueba</h1>
     );
